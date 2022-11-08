be862cc150113a8e359d03492b4e0727a24cb357
Fioenix <phuong.tang@hoang-phuc.net>
2022-11-08 10:59:23 +0700

    upgrade log.go

--- log.go
+++ log.go
@@ -51,6 +51,11 @@ func Debug(args ...interface{}) {
 	log.Debug(args...)
 }
 
+// Print logs a message at level Info on the standard logger.
+func Print(args ...interface{}) {
+	log.Print(args...)
+}
+
 // Info logs a message at level Info on the standard logger.
 func Info(args ...interface{}) {
 	log.Info(args...)
@@ -84,6 +89,11 @@ func Tracef(format string, args ...interface{}) {
 // Debugf logs a message at level Debug on the standard logger.
 func Debugf(format string, args ...interface{}) {
 	log.Debugf(format, args...)
+}
+
+// Printf logs a message at level Info on the standard logger.
+func Printf(format string, args ...interface{}) {
+	log.Printf(format, args...)
 }
 
 // Infof logs a message at level Info on the standard logger.
